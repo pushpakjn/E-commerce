34d97d8a090dd9f4fb2561cc90ad9fe780916d31
Pushpak Jain <officialpjain@gmail.com>
2023-10-02 19:45:54 +0530

    upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import { Outlet } from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <>
+      <Header/>
+        <main className="py-3">
+          <Container>
+          <Outlet/>
+          </Container>
+        </main>
+        <Footer/>
+        <ToastContainer/>
+    </>
   );
 }
 
---
+++ frontend/src/products.js
@@ -0,0 +1,41 @@
+const products = [
+    {
+        _id: 1,
+        name: 'Camera',
+        price: 1000,
+        image: '/images/camera.jpg',
+        description: 'A Camera for cool people',
+        brand: 'Sony',
+        price: 1000,
+        rating: 4.5,
+        numReviews: 10,
+        countInStock: 12
+    },
+    {
+        _id: 2,
+        name: 'iPhone',
+        price: 800,
+        image: '/images/phone.jpg',
+        description: 'A phone for cool people',
+        brand: 'Apple',
+        price: 100,
+        rating: 4.1,
+        numReviews: 8,
+        countInStock: 0
+    },
+    {
+        _id: 3,
+        name: 'Airpods',
+        price: 2000,
+        image: '/images/airpods.jpg',
+        description: 'A airpods for cool people',
+        brand: 'Apple',
+        price: 2000,
+        rating: 4.9,
+        numReviews: 12,
+        countInStock: 2
+    }
+
+];
+
+export default products;
