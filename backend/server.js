fe4952fa4294a52bce1014b72ac4fa4e543dedcd
Pushpak Jain <84683252+pushpakjn@users.noreply.github.com>
2023-10-03 16:57:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === 'production') {
   );
 } else {
   const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use('/uploads',  express.static('/tmp/uploads'));
   app.get('/', (req, res) => {
     res.send('API is running....');
   });
